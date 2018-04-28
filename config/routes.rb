e033f413774a7ebe0ff9665b3d365f42a78c4358
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2018-04-27 20:45:09 -0400

    carrusel

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :recursos
   resources :funcionarios
   resources :locations
-  root "welcomes#index"
+  root "welcomes#new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
